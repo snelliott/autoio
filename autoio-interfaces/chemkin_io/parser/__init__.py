9916433f84c9352bc85f660636778155c31a93fd
cmulvihill <crm7025@gmail.com>
2021-07-22 15:43:13 -0500

    removing chemkin_io/parser/files.py

--- autoio-interfaces/chemkin_io/parser/__init__.py
+++ autoio-interfaces/chemkin_io/parser/__init__.py
@@ -6,7 +6,6 @@ from chemkin_io.parser import mechanism
 from chemkin_io.parser import reaction
 from chemkin_io.parser import species
 from chemkin_io.parser import thermo
-from chemkin_io.parser import files
 
 
 __all__ = [
@@ -14,5 +13,4 @@ __all__ = [
     'reaction',
     'species',
     'thermo',
-    'files'
 ]
