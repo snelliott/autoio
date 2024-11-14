0edd7798a82677afe9d203d9566063b237a9f9db
Luna Pratali Maffei <luna.pratali@polimi.it>
2024-11-14 16:46:02 +0100

    scientific format for ene reading allowed

--- autoread/energy.py
+++ autoread/energy.py
@@ -6,7 +6,9 @@ import autoparse.find as apf
 import autoparse.pattern as app
 
 
-VALUE_PATTERN = app.one_of_these([app.EXPONENTIAL_FLOAT_D, app.FLOAT])
+VALUE_PATTERN = app.one_of_these([app.EXPONENTIAL_FLOAT, 
+                                  app.EXPONENTIAL_FLOAT_D, 
+                                  app.FLOAT])
 SEP_PATTERN = app.LINESPACES
 
 
@@ -30,7 +32,6 @@ def read(string,
     """
 
     ptt_ = pattern(start_ptt=start_ptt, val_ptt=app.capturing(val_ptt))
-
     ene_str = (apf.last_capture(ptt_, string, case=case) if last else
                apf.first_capture(ptt_, string, case=case))
     if ene_str is not None:
